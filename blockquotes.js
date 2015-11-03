df485249f7f0b5c118803e2edf3fb3313fdc662a
Devin Price <devin@wptheming.com>
2015-11-02 18:02:44 -0600

    Add default action for selected text.

--- blockquotes.js
+++ blockquotes.js
@@ -1,8 +1,7 @@
 /*
  * Adds a new custom blockquote button.
- * When clicked, a pop-up will display options for quote, citation, citation link.
- *
- * @TODO Make strings translatable
+ * If text is selected, the button will toggle blockquote markup around the selection.
+ * If no text is selected, a pop-up will display options for quote, citation, citation link.
  */
 
 (function() {
@@ -14,50 +13,60 @@
             icon: 'blockquote',
             onclick: function() {
 
-				editor.windowManager.open({
-				    title: better_blockquotes.blockquote,
-				    body: [
-				    {
-				        type: 'textbox',
-				        name: 'quote',
-				        label: better_blockquotes.quote,
-				        multiline: true,
-				        minWidth: 300,
-						minHeight: 100
-				    },
-					{
-				        type: 'textbox',
-				        name: 'cite',
-				        label: better_blockquotes.citation,
-				    },
-					{
-				        type: 'textbox',
-				        name: 'link',
-				        label: better_blockquotes.citation_link,
-				    },
-				    ],
-				    onsubmit: function( e ) {
+				// If text is selected, toggle blockquote markup
+	            if ( editor.selection.getContent() ) {
 
-					    var blockquote = '';
-					    var cite = '';
+					editor.formatter.toggle('blockquote');
 
-              if ( e.data.link && e.data.cite ) {
-                cite = '<a href="' + e.data.link + '">' + e.data.cite + '</a>';
-              } else if ( !e.data.link && e.data.cite ) {
-                cite = '<cite>' + e.data.cite + '</cite>';
-              }
+				// If no text is selected, display pop-up
+	            } else {
 
-  						if ( e.data.quote ) {
-  							blockquote += '<blockquote>';
-  							blockquote += e.data.quote;
-  							blockquote += cite;
-  							blockquote += '</blockquote>';
+					editor.windowManager.open({
+					    title: better_blockquotes.blockquote,
+					    body: [
+					    {
+					        type: 'textbox',
+					        name: 'quote',
+					        label: better_blockquotes.quote,
+					        multiline: true,
+					        minWidth: 300,
+							minHeight: 100
+					    },
+						{
+					        type: 'textbox',
+					        name: 'cite',
+					        label: better_blockquotes.citation,
+					    },
+						{
+					        type: 'textbox',
+					        name: 'link',
+					        label: better_blockquotes.citation_link,
+					    },
+					    ],
+					    onsubmit: function( e ) {
+
+						    var blockquote = '';
+						    var cite = '';
+
+							if ( e.data.link && e.data.cite ) {
+								cite = '<cite><a href="' + e.data.link + '">' + e.data.cite + '</a></cite>';
+	              			} else if ( !e.data.link && e.data.cite ) {
+				  				cite = '<cite>' + e.data.cite + '</cite>';
+	              			}
+
+	  						if ( e.data.quote ) {
+	  							blockquote += '<blockquote>';
+	  							blockquote += e.data.quote;
+	  							blockquote += cite;
+	  							blockquote += '</blockquote>';
+						    }
+
+					        editor.insertContent(blockquote);
+
 					    }
+					});
 
-				        editor.insertContent(blockquote);
-
-				    }
-				});
+				}
 			}
         });
 
